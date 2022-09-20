9f28c683ddefea4d6032d196e8f8c7cba211773a
Mati Rossi <mati.rossi@weareadaptive.com>
2022-09-20 09:00:02 +0200

    refactor: delete unused variables and refactor functions

--- index.js
+++ index.js
@@ -1,82 +1,68 @@
-let [tensCounter,secondsCounter, minutesCounter] = [0, 0, 0];
-const lapCounters = {tensCounter: 0, secondsCounter: 0, minutesCounter: 0};
-let lapStartDate;
-let elapsedTime = 0;
-let interval, lapInterval;
+const counters = {hundredsCounter: 0,secondsCounter: 0, minutesCounter: 0}
+const lapCounters = {hundredsCounter: 0, secondsCounter: 0, minutesCounter: 0};
+let lapStartDate, startDate;
+let [elapsedTime, elapsedLapTime] = [0, 0];
+let interval;
 let isTimerRunning = false;
-const $tens = document.getElementById("tens");
-const $seconds = document.getElementById("seconds");
-const $minutes = document.getElementById("minutes");
+const $timer = document.getElementById("timer");
 const $buttonRight = document.getElementById("button-right");
 const $buttonLeft = document.getElementById("button-left");
 const $lapsList = document.getElementById("laps-list");
 const sixEmptyLaps = $lapsList.innerHTML;
 
-const runTimer = () => {
-    tensCounter++;
-    if (tensCounter > 9){
-        $tens.innerHTML = tensCounter;
-    } else {
-        $tens.innerHTML = "0" + tensCounter;
-    }
-    if (tensCounter === 99) {
-        tensCounter = 0;
-        secondsCounter++;
-        if (secondsCounter < 10) {
-            $seconds.innerHTML = "0" + secondsCounter;
-        } else if (secondsCounter === 60) {
-            $seconds.innerHTML = "00";
-        } else {
-            $seconds.innerHTML = secondsCounter;
-        }
-    }
-    if (secondsCounter === 60)
-    { 
-        secondsCounter = 0;
-        minutesCounter++;
-        if (minutesCounter < 10) {
-            $minutes.innerHTML = `0${minutesCounter}`;
-        } else {
-            $minutes.innerHTML = minutesCounter;
-        } 
-    }
-}
 
 const createNewLap = () => {
     const newLap = document.createElement("li");
-    newLap.classList.add("lap")
+    newLap.classList.add("lap");
     $lapsList.insertBefore(newLap, $lapsList.firstChild);
     ($lapsList.lastElementChild.innerHTML.trim() === "") && $lapsList.lastElementChild.remove();
 }
-const updateLapNode = (lapObj) => {
-    const tens = lapObj.tensCounter > 9 ? lapObj.tensCounter :  `0${lapObj.tensCounter}`;
-    const seconds = lapObj.secondsCounter > 9 ? lapObj.secondsCounter :  `0${lapObj.secondsCounter}`;
-    const minutes = lapObj.minutesCounter < 9 ? `0${lapObj.minutesCounter}` : lapObj.minutesCounter ;
-    $lapsList.firstElementChild.innerHTML = `<span>Lap</span><span>${minutes}:${seconds}.${tens}</span>`;
+
+const updateTimerNode = () => {
+    const hundreds = counters.hundredsCounter > 9 ? counters.hundredsCounter :  `0${counters.hundredsCounter}`;
+    const seconds = counters.secondsCounter > 9 ? counters.secondsCounter :  `0${counters.secondsCounter}`;
+    const minutes = counters.minutesCounter < 9 ? `0${counters.minutesCounter}` : counters.minutesCounter ;
+    $timer.innerText = `${minutes}:${seconds}.${hundreds}`;
 }
 
-const updateLapTimer = (lapObj) => {
-    const temporalElapsedTime = Date.now() - lapStartDate + elapsedTime;
-    lapObj.tensCounter = temporalElapsedTime % 100;
-    lapObj.secondsCounter = Math.floor(temporalElapsedTime / 1000) % 60;
-    lapObj.minutesCounter = Math.floor((temporalElapsedTime / 1000) / 60) % 60;
-    updateLapNode(lapObj);
+const updateLapNode = () => {
+    const hundreds = lapCounters.hundredsCounter > 9 ? lapCounters.hundredsCounter :  `0${lapCounters.hundredsCounter}`;
+    const seconds = lapCounters.secondsCounter > 9 ? lapCounters.secondsCounter :  `0${lapCounters.secondsCounter}`;
+    const minutes = lapCounters.minutesCounter < 9 ? `0${lapCounters.minutesCounter}` : lapCounters.minutesCounter ;
+    $lapsList.firstElementChild.innerHTML = `<span>Lap</span><span>${minutes}:${seconds}.${hundreds}</span>`;
 }
+
+const updateCounters = (temporalElapsedTime, countersObject) => {
+    countersObject.hundredsCounter = Math.floor(temporalElapsedTime / 10) % 100;
+    countersObject.secondsCounter = Math.floor(temporalElapsedTime / 1000) % 60;
+    countersObject.minutesCounter = Math.floor((temporalElapsedTime / 1000) / 60) % 60;
+}
+
+const updateTimer = () => {
+    const currentDate = Date.now();
+    const temporalElapsedTime = currentDate - startDate + elapsedTime;
+    const lapTemporalElapsedTime = currentDate - lapStartDate + elapsedLapTime;
+    updateCounters(temporalElapsedTime, counters);
+    updateCounters(lapTemporalElapsedTime, lapCounters);
+    updateTimerNode();
+    updateLapNode(); 
+}
+
 
 $buttonRight.onclick = () => {   
     if (!isTimerRunning){
         $lapsList.firstElementChild.innerHTML.trim() === "" && createNewLap();
+        startDate = Date.now();
         lapStartDate = Date.now();
-        $buttonLeft.innerHTML = "Lap";
-        $buttonRight.innerHTML = "stop";
-        isTimerRunning = true;
+        $buttonRight.innerText = "stop";
+        $buttonLeft.innerText = "Lap";
         $buttonLeft.disabled = false;
-        interval = setInterval(runTimer,10);
-        lapInterval = setInterval(updateLapTimer, 1, lapCounters);
+        interval = setInterval(updateTimer,10);
+        isTimerRunning = true;
     } else {
         clearInterval(interval);
-        clearInterval(lapInterval);
-        elapsedTime += Date.now() - lapStartDate;
+        elapsedTime += Date.now() - startDate;
+        elapsedLapTime +=Date.now() - lapStartDate;
         $buttonLeft.innerHTML = "Reset"; 
         $buttonRight.innerHTML = "Start";
         isTimerRunning = false;
@@ -84,21 +70,16 @@ $buttonRight.onclick = () => {
 };
 
 $buttonLeft.addEventListener("click", () => {
-    if ($buttonLeft.innerHTML === "Reset") {
+    if ($buttonLeft.innerText === "Reset") {
         $buttonLeft.disabled = true;
-        $buttonLeft.innerHTML = "Lap";
-        [tensCounter,secondsCounter, minutesCounter] = [0, 0, 0]; 
-        [$minutes.innerHTML, $seconds.innerHTML, $tens.innerHTML] = ["00", "00", "00"];
-        elapsedTime = 0;
+        $buttonLeft.innerText = "Lap";
+        [hundredsCounter,secondsCounter, minutesCounter] = [0, 0, 0]; 
+        $timer.innerText = "00:00.00";
+        [elapsedTime, elapsedLapTime] = [0, 0];
         $lapsList.innerHTML = sixEmptyLaps;
-    } else if($buttonLeft.innerHTML === "Lap"){
-        elapsedTime = 0;
+    } else if($buttonLeft.innerText === "Lap"){
+        elapsedLapTime = 0;
         lapStartDate = Date.now();
-        createNewLap();
+        createNewLap()
     }
 });
-
-
-const increment = (num) => {
-    num++
-}
